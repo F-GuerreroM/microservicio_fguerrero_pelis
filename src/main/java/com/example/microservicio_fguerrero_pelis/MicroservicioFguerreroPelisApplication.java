40491a61df1a64edea316a817f6f78fe107d04d5
Felipe Antonio Guerrero Muñoz <fguerrero@sence.cl>
2025-03-24 22:31:02 -0300

    first commit

---
+++ src/main/java/com/example/microservicio_fguerrero_pelis/MicroservicioFguerreroPelisApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_fguerrero_pelis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioFguerreroPelisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioFguerreroPelisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservicio_fguerrero_pelis/MicroservicioFguerreroPelisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_fguerrero_pelis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioFguerreroPelisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
